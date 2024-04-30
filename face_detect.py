5192c8c29409d5750ffe454b633e14e3498e0d01
Adomas Dagys <puslys.adomas@gmail.com>
2024-05-01 00:54:57 +0200

    Implement face detection (haar)

---
+++ face_detect.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+from rescale_utils import rescale_frames
+
+img_fullsize = cv.imread('Photos/faces (2).jpg')
+img = rescale_frames(img_fullsize)
+cv.imshow('Cat', img)
+
+# based on edges the face is detected.
+# first turn to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# classifier reads XML code and stores it in variable haar_cascade
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+# returns coordinates of the face as array
+# minNeighbors=3 is the number of neighbors rectangle should have to be a face
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+# we can loop through:
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+
+cv.imshow('Detected faces', img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
